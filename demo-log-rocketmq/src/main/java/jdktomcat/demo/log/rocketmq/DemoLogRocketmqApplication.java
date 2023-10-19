415595e19eefa08647d4d808e52366cc1ff055d9
Timmy <jdktomcat@163.com>
2023-10-19 17:38:37 +0800

    测试

---
+++ demo-log-rocketmq/src/main/java/jdktomcat/demo/log/rocketmq/DemoLogRocketmqApplication.java
@@ -0,0 +1,19 @@
+package jdktomcat.demo.log.rocketmq;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLogRocketmqApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLogRocketmqApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) {
+
+    }
+}
