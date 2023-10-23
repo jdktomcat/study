257dad96e50ec4d341dc64cbacba68ce50163252
jdktomcat <jdktomcat@163.com>
2023-10-23 22:42:14 +0800

    代码优化

--- demo-log-redis/src/main/java/jdktomcat/log/demo/redis/LogDemoRedisApplication.java
+++ demo-log-redis/src/main/java/jdktomcat/log/demo/redis/LogDemoRedisApplication.java
@@ -1,5 +1,8 @@
 package jdktomcat.log.demo.redis;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +11,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 import java.time.Duration;
 
+@Slf4j
 @SpringBootApplication
 public class LogDemoRedisApplication implements CommandLineRunner {
 
@@ -20,13 +24,14 @@ public class LogDemoRedisApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        redisTemplate.opsForValue().setIfAbsent("bbb", "ddd");
+        Boolean result = redisTemplate.opsForValue().setIfAbsent("bbb", "ddd");
+        log.info("save key result:{}",result);
         redisTemplate.opsForValue().set("aaa", "bbb", Duration.ofMinutes(1));
         Object ret = redisTemplate.opsForValue().get("aaa");
-
-        redisTemplate.delete("bbb");
-
-        redisTemplate.opsForHash().get("abc", "def");
-        //System.out.println("redis操作返回: " + ret);
+        log.info("get key result:{}",ret);
+        Boolean deleteResult = redisTemplate.delete("bbb");
+        log.info("delete key result:{}",deleteResult);
+        Object hashRet = redisTemplate.opsForHash().get("abc", "def");
+        log.info("get hash key result:{}",hashRet);
     }
 }
