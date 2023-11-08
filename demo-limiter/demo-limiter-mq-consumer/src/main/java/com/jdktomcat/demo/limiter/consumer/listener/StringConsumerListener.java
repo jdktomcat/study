5170ef04796174eff5560f7ac6f6ce88538f18d6
Timmy <jdktomcat@163.com>
2023-11-08 20:34:19 +0800

    测试

---
+++ demo-limiter/demo-limiter-mq-consumer/src/main/java/com/jdktomcat/demo/limiter/consumer/listener/StringConsumerListener.java
@@ -0,0 +1,21 @@
+package com.jdktomcat.demo.limiter.consumer.listener;
+
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ZF-Timmy
+ * @version V1.0
+ * @description 类描述：消费者
+ * @date 2023/11/8
+ */
+
+@Service
+@RocketMQMessageListener(topic = "${demo.rocketmq.topic}", consumerGroup = "timmy_consumer", selectorExpression = "${demo.rocketmq.tag}", tlsEnable = "${demo.rocketmq.tlsEnable}")
+public class StringConsumerListener implements RocketMQListener<String> {
+    @Override
+    public void onMessage(String s) {
+
+    }
+}
