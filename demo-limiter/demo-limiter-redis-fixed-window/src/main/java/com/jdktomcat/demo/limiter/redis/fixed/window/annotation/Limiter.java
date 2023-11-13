d8b5e41e22fa6b0de3bb3eb5a989453f0d4f824b
Timmy <jdktomcat@163.com>
2023-11-13 21:54:22 +0800

    限流自测

---
+++ demo-limiter/demo-limiter-redis-fixed-window/src/main/java/com/jdktomcat/demo/limiter/redis/fixed/window/annotation/Limiter.java
@@ -0,0 +1,38 @@
+package com.jdktomcat.demo.limiter.redis.fixed.window.annotation;
+
+
+import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 自定义注解限流
+ *
+ * @author jstimmy
+ */
+@Inherited
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Limiter {
+
+    /**
+     * 限流值
+     *
+     * @return 值
+     */
+    int value() default 5;
+
+    /**
+     * 时间单位
+     *
+     * @return 时间单位
+     */
+    TimeUnit unit() default TimeUnit.SECONDS;
+
+    /**
+     * 时间窗口
+     *
+     * @return 时间窗口
+     */
+    int window() default 10;
+}
--- demo-limiter/demo-limiter-redis-fixed-window/src/main/java/com/jdktomcat/demo/limiter/redis/fixed/window/controller/TestController.java
+++ demo-limiter/demo-limiter-redis-fixed-window/src/main/java/com/jdktomcat/demo/limiter/redis/fixed/window/controller/TestController.java
@@ -1,10 +1,23 @@
 package com.jdktomcat.demo.limiter.redis.fixed.window.controller;
 
+import com.jdktomcat.demo.limiter.redis.fixed.window.annotation.Limiter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author ZF-Timmy
  * @version V1.0
  * @description 类描述：
  * @date 2023/11/13
  */
+@RestController
 public class TestController {
+
+    @Limiter(value = 10, unit = TimeUnit.MINUTES, window = 1)
+    @GetMapping("/limiter")
+    public String say(String name) {
+        return "hello" + name;
+    }
 }
