19d0024f91ef7dacaf28f845ea7f4a6bf9560ea8
Timmy <jdktomcat@163.com>
2023-11-13 20:40:51 +0800

    限流自测

---
+++ demo-limiter/demo-limiter-redis-fixed-window/src/main/java/com/jdktomcat/demo/limiter/redis/fixed/window/App.java
@@ -0,0 +1,13 @@
+package com.jdktomcat.demo.limiter.redis.fixed.window;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
