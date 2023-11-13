0ed483e737d2fc5ffa7254b294a9511d703146d4
Timmy <jdktomcat@163.com>
2023-11-13 21:43:39 +0800

    限流自测

---
+++ demo-limiter/demo-limiter-redis-fixed-window/src/main/java/com/jdktomcat/demo/limiter/redis/fixed/window/DemoLimiterRedisFixedWindowApplication.java
@@ -0,0 +1,19 @@
+package com.jdktomcat.demo.limiter.redis.fixed.window;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZF-Timmy
+ * @version V1.0
+ * @description 类描述：
+ * @date 2023/11/13
+ */
+@SpringBootApplication
+public class DemoLimiterRedisFixedWindowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLimiterRedisFixedWindowApplication.class, args);
+    }
+
+}
---
+++ demo-limiter/demo-limiter-redis-fixed-window/src/main/java/com/jdktomcat/demo/limiter/redis/fixed/window/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.jdktomcat.demo.limiter.redis.fixed.window.controller;
+
+/**
+ * @author ZF-Timmy
+ * @version V1.0
+ * @description 类描述：
+ * @date 2023/11/13
+ */
+public class TestController {
+}
