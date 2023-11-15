e859bf8f68208d50e9216b6849538a52681d1d96
Timmy <jdktomcat@163.com>
2023-11-15 19:14:10 +0800

    nacos

---
+++ demo-nacos/demo-nacos-application/src/main/java/com/jdktomcat/demo/nacos/application/DemoNacosApplication.java
@@ -0,0 +1,13 @@
+package com.jdktomcat.demo.nacos.application;
+
+/**
+ * Hello world!
+ *
+ */
+public class DemoNacosApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
