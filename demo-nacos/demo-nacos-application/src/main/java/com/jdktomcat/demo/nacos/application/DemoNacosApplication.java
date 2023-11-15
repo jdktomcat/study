4c6819aff1764eddcbd873c38b2636627bff7a9c
Timmy <jdktomcat@163.com>
2023-11-15 19:59:24 +0800

    nacos

--- demo-nacos/demo-nacos-application/src/main/java/com/jdktomcat/demo/nacos/application/DemoNacosApplication.java
+++ demo-nacos/demo-nacos-application/src/main/java/com/jdktomcat/demo/nacos/application/DemoNacosApplication.java
@@ -1,24 +1,17 @@
 package com.jdktomcat.demo.nacos.application;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
- *
  */
-
 
 @Slf4j
 @SpringBootApplication
-public class DemoNacosApplication implements CommandLineRunner {
-    public static void main( String[] args ) {
-        
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
+public class DemoNacosApplication{
+    public static void main(String[] args) {
+        SpringApplication.run(DemoNacosApplication.class, args);
     }
 }
