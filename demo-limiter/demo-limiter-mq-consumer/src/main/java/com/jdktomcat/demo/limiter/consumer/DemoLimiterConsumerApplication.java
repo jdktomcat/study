60db1c218b31e9039cf2d41e975a8fa75315d4bd
timmy <jdktomcat@163.com>
2023-11-27 21:31:15 +0800

    代码优化

--- demo-limiter/demo-limiter-mq-consumer/src/main/java/com/jdktomcat/demo/limiter/consumer/DemoLimiterConsumerApplication.java
+++ demo-limiter/demo-limiter-mq-consumer/src/main/java/com/jdktomcat/demo/limiter/consumer/DemoLimiterConsumerApplication.java
@@ -1,6 +1,5 @@
 package com.jdktomcat.demo.limiter.consumer;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
