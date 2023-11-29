d6b9fbcdbe82b4bfde729082e105f77ed3047855
Timmy <jdktomcat@163.com>
2023-11-29 20:35:02 +0800

    limiter

--- demo-limiter/demo-limiter-mq-consumer/src/main/java/com/jdktomcat/demo/limiter/consumer/listener/StringConsumerListener.java
+++ demo-limiter/demo-limiter-mq-consumer/src/main/java/com/jdktomcat/demo/limiter/consumer/listener/StringConsumerListener.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.jdktomcat.demo.limiter.consumer.component.HttpComponent;
 import com.jdktomcat.demo.limiter.consumer.component.RedisComponent;
 import com.jdktomcat.demo.limiter.consumer.message.AlertMessage;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
@@ -13,12 +14,9 @@ import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Set;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author ZF-Timmy
@@ -33,11 +31,11 @@ public class StringConsumerListener implements RocketMQListener<String> {
 
     private final ScheduledExecutorService consumerExecutor = new ScheduledThreadPoolExecutor(1);
 
+    private final ThreadPoolExecutor taskPoolExecutor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), Runtime.getRuntime().availableProcessors() + 5, 5L, TimeUnit.SECONDS, new LinkedBlockingDeque<>(1000), new ThreadPoolExecutor.CallerRunsPolicy());
 
     private static final String URL = "https://api.telegram.org/bot";
 
     private static final String API = "/sendMessage";
-
 
     @Autowired
     private RedisComponent redisComponent;
@@ -57,6 +55,56 @@ public class StringConsumerListener implements RocketMQListener<String> {
 
     private static final String BOT_CHAT_LOCK = "telegram:consume:lock:%s:%s";
 
+    @Data
+    public static class SendTask implements Runnable {
+
+        private String bot;
+
+        private String chat;
+
+        private RedisComponent redisComponent;
+
+        private HttpComponent httpComponent;
+
+        public SendTask(String bot, String chat, RedisComponent redisComponent, HttpComponent httpComponent) {
+            this.bot = bot;
+            this.chat = chat;
+            this.redisComponent = redisComponent;
+            this.httpComponent = httpComponent;
+        }
+
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @Override
+        public void run() {
+            if (redisComponent.listLength(String.format(BOT_CHAT_MESSAGE_LIST, bot, chat)) == 0L) {
+                return;
+            }
+            if (!redisComponent.limitFlowFixedWindow(String.format(BOT_CHAT_EIGENVALUE, bot, chat), 20, 60)) {
+                return;
+            }
+            String message = redisComponent.pop(String.format(BOT_CHAT_MESSAGE_LIST, bot, chat));
+            if (StringUtils.isEmpty(message)) {
+                return;
+            }
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("chat_id", chat);
+            jsonObject.put("parse_mode", "HTML");
+            jsonObject.put("text", message);
+            String result = httpComponent.postForJson(URL + bot + API, new HashMap<>(), jsonObject.toJSONString());
+            log.info("telegram send msg success! url:{} data:{} result:{}", URL + bot + API, jsonObject.toJSONString(), result);
+        }
+    }
+
     /**
      * 消费电报消息
      */
@@ -70,22 +118,7 @@ public class StringConsumerListener implements RocketMQListener<String> {
                 }
                 Set<String> botChatSet = redisComponent.getSetMember(String.format(BOT_CHAT_SET, bot));
                 for (String chat : botChatSet) {
-                    if (redisComponent.listLength(String.format(BOT_CHAT_MESSAGE_LIST, bot, chat)) == 0L) {
-                        continue;
-                    }
-                    if (!redisComponent.limitFlowFixedWindow(String.format(BOT_CHAT_EIGENVALUE, bot, chat), 20, 60)) {
-                        continue;
-                    }
-                    String message = redisComponent.pop(String.format(BOT_CHAT_MESSAGE_LIST, bot, chat));
-                    if (StringUtils.isEmpty(message)) {
-                        continue;
-                    }
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("chat_id", chat);
-                    jsonObject.put("parse_mode", "HTML");
-                    jsonObject.put("text", message);
-                    String result = httpComponent.postForJson(URL + bot + API, new HashMap<>(), jsonObject.toJSONString());
-                    log.info("telegram send msg success! url:{} data:{} result:{}", URL + bot + API, jsonObject.toJSONString(), result);
+                    taskPoolExecutor.execute(new SendTask(bot, chat, redisComponent, httpComponent));
                 }
             }
         }, 0, 30, TimeUnit.MILLISECONDS);
@@ -101,10 +134,10 @@ public class StringConsumerListener implements RocketMQListener<String> {
         AlertMessage alertMessage = JSONObject.parseObject(message, AlertMessage.class);
         if (alertMessage != null && !StringUtils.isEmpty(alertMessage.getBot()) && !StringUtils.isEmpty(alertMessage.getChat())) {
             String lockKey = String.format(BOT_CHAT_LOCK, alertMessage.getBot(), alertMessage.getChat());
-            while(!redisComponent.getLock(lockKey)){
-                try{
+            while (!redisComponent.getLock(lockKey)) {
+                try {
                     TimeUnit.MILLISECONDS.sleep(300L);
-                }catch (Exception exception){
+                } catch (Exception exception) {
                     log.warn("线程休眠异常！", exception);
                 }
             }
