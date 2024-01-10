5b63ecfc21b320b61dbe3ae226eb2e583d20ca95
Timmy <jdktomcat@163.com>
2024-01-10 20:48:26 +0800

    learn-develop-skywalking

---
+++ demo-skywalking/demo-skywalking-gateway/demo-skywalking-gateway-route/src/main/java/jdktomcat/demo/skywalking/gateway/route/filter/ResetTraceIdFilter.java
@@ -0,0 +1,74 @@
+package jdktomcat.demo.skywalking.gateway.route.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author ZF-Timmy
+ * @version V1.0
+ * @description 类描述：traceId拦截器
+ * @date 2024/1/8
+ */
+@Component
+@Slf4j
+public class ResetTraceIdFilter implements GlobalFilter, Ordered {
+
+    @Value("${resetTraceIdUris:/say}")
+    private String resetTraceIdUris;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String uri = request.getURI().toString();
+        log.info("request uri:{}", uri);
+        if (matchContext(uri)) {
+            Map<String, List<String>> headers = request.getHeaders();
+            headers.forEach((key, dataList) -> {
+                if (key.contains("sw")) {
+                    request.getHeaders().replace(key, null);
+                    log.info("request reset head param key:{} value:{}",key, Arrays.toString(dataList.toArray()));
+                }
+            });
+            ServerHttpRequest newRequest = request.mutate().build();
+            ServerWebExchange nExchange = exchange.mutate().request(newRequest).build();
+            return chain.filter(nExchange);
+        }
+        return chain.filter(exchange);
+    }
+
+    private boolean matchContext(String uri) {
+        if (StringUtils.isEmpty(resetTraceIdUris)) {
+            return false;
+        }
+        return Arrays.stream(resetTraceIdUris.split(",")).anyMatch(uri::contains);
+    }
+
+    /**
+     * Get the order value of this object.
+     * <p>Higher values are interpreted as lower priority. As a consequence,
+     * the object with the lowest value has the highest priority (somewhat
+     * analogous to Servlet {@code load-on-startup} values).
+     * <p>Same order values will result in arbitrary sort positions for the
+     * affected objects.
+     *
+     * @return the order value
+     * @see #HIGHEST_PRECEDENCE
+     * @see #LOWEST_PRECEDENCE
+     */
+    @Override
+    public int getOrder() {
+        return Integer.MIN_VALUE;
+    }
+}
