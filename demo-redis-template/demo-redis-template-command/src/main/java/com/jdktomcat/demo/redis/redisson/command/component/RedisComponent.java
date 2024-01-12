830a27312e85b7ee94c626265ff5f989a7d3e7e5
Timmy <jdktomcat@163.com>
2024-01-12 20:42:08 +0800

    learn-develop-redisson

---
+++ demo-redis-template/demo-redis-template-command/src/main/java/com/jdktomcat/demo/redis/redisson/command/component/RedisComponent.java
@@ -0,0 +1,37 @@
+package com.jdktomcat.demo.redis.redisson.command.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RScoredSortedSet;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ZF-Timmy
+ * @version V1.0
+ * @description 类描述：redis组件
+ * @date 2023/11/8
+ */
+@Slf4j
+@Component
+public class RedisComponent {
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    public Object zPopMin(String key) {
+        return redissonClient.getScoredSortedSet(key).pollFirst();
+    }
+
+    public double firstScore(String key) {
+        return redissonClient.getScoredSortedSet(key).firstScore();
+    }
+
+    public boolean zadd(String key, Object field, double score) {
+        return redissonClient.getScoredSortedSet(key).add(score, field);
+    }
+
+    public boolean del(String key) {
+        return redissonClient.getScoredSortedSet(key).delete();
+    }
+}
