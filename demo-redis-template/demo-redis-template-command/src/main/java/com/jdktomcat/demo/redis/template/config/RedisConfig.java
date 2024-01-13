e492bdd501eaa1de8fbf2625e8e3ef911390935e
Timmy <jdktomcat@163.com>
2024-01-13 13:33:11 +0800

    learn-develop-template

--- demo-redis-template/demo-redis-template-command/src/main/java/com/jdktomcat/demo/redis/template/config/RedisConfig.java
+++ demo-redis-template/demo-redis-template-command/src/main/java/com/jdktomcat/demo/redis/template/config/RedisConfig.java
@@ -10,14 +10,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
-@ConditionalOnClass(RedisOperations.class)
+@ConditionalOnClass(RedisTemplate.class)
 @EnableConfigurationProperties(RedisProperties.class)
 public class RedisConfig {
 
