c6c835809c93aa446322ae014fb90c6e0a25a4f4
timmy <jdktomcat@163.com>
2024-02-11 19:52:00 +0800

    mybatis

--- demo-dead-lock/demo-dead-lock-restart-transaction/src/main/java/com/jdktomcat/demo/dead/lock/restart/transaction/config/MybatisPlusConfig.java
+++ demo-dead-lock/demo-dead-lock-restart-transaction/src/main/java/com/jdktomcat/demo/dead/lock/restart/transaction/config/MybatisPlusConfig.java
@@ -1,15 +1,7 @@
 package com.jdktomcat.demo.dead.lock.restart.transaction.config;
 
-import com.baomidou.mybatisplus.core.MybatisConfiguration;
-import com.baomidou.mybatisplus.extension.MybatisMapWrapperFactory;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.type.JdbcType;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 /**
  * Mybatis Plus Config
@@ -18,16 +10,4 @@ import javax.sql.DataSource;
 @MapperScan("com.jdktomcat.demo.dead.lock.restart.transaction.mapper")
 public class MybatisPlusConfig {
 
-    @Bean("mybatisSqlSession")
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
-        //数据源
-        sqlSessionFactory.setDataSource(dataSource);
-        MybatisConfiguration configuration = new MybatisConfiguration();
-        configuration.setJdbcTypeForNull(JdbcType.NULL);
-        /* map 下划线转驼峰 */
-        configuration.setObjectWrapperFactory(new MybatisMapWrapperFactory());
-        sqlSessionFactory.setConfiguration(configuration);
-        return sqlSessionFactory.getObject();
-    }
 }
