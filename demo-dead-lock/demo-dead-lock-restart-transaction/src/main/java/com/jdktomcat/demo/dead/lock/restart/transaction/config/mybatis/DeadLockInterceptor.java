a3badea846433b6ebfa26af624a76ca2d7b4a627
timmy <jdktomcat@163.com>
2024-02-12 15:47:21 +0800

    mybatis

--- demo-dead-lock/demo-dead-lock-restart-transaction/src/main/java/com/jdktomcat/demo/dead/lock/restart/transaction/config/mybatis/DeadLockInterceptor.java
+++ demo-dead-lock/demo-dead-lock-restart-transaction/src/main/java/com/jdktomcat/demo/dead/lock/restart/transaction/config/mybatis/DeadLockInterceptor.java
@@ -2,14 +2,19 @@ package com.jdktomcat.demo.dead.lock.restart.transaction.config.mybatis;
 
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.mysql.cj.jdbc.exceptions.MySQLTransactionRollbackException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.transaction.Transaction;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
 
 /**
  * mybatis死锁拦截器
@@ -24,12 +29,30 @@ import java.lang.reflect.InvocationTargetException;
 public class DeadLockInterceptor extends MybatisPlusInterceptor {
 
     @Override
-    public Object intercept(Invocation invocation) throws InvocationTargetException, IllegalAccessException {
+    public Object intercept(Invocation invocation) throws InvocationTargetException, IllegalAccessException, SQLException {
         try{
+
             return invocation.proceed();
-        }catch (Exception exception){
+        }catch (InvocationTargetException exception){
             log.error("数据库更新异常！", exception);
-            throw exception;
+            return restartTransaction(invocation, exception);
         }
     }
+
+
+    private Object restartTransaction(Invocation invocation, InvocationTargetException exception) throws InvocationTargetException, SQLException {
+        Throwable throwable = exception.getTargetException();
+        if(throwable instanceof MySQLTransactionRollbackException){
+            MySQLTransactionRollbackException transactionRollbackException = (MySQLTransactionRollbackException)throwable;
+            if(transactionRollbackException.getSQLState().equalsIgnoreCase("40001")){
+                // 死锁场景处理
+                log.info("即将回滚事务：{}", TransactionSynchronizationManager.getCurrentTransactionName());
+                log.info("即将回滚事务中的SQL命令列表：{}", TransactionSynchronizationManager.getSynchronizations().toArray(new TransactionSynchronization[0]));
+            }
+        }
+        throw exception;
+    }
+
+
+
 }
