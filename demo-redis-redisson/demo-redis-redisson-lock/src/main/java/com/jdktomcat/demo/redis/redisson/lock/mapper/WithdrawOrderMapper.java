9f85df052c3c49cb03ee8b603687f01729d49f4d
timmy <jdktomcat@163.com>
2024-02-14 18:56:22 +0800

    redis锁失效

--- demo-redis-redisson/demo-redis-redisson-lock/src/main/java/com/jdktomcat/demo/redis/redisson/lock/mapper/WithdrawOrderMapper.java
+++ demo-redis-redisson/demo-redis-redisson-lock/src/main/java/com/jdktomcat/demo/redis/redisson/lock/mapper/WithdrawOrderMapper.java
@@ -2,6 +2,14 @@ package com.jdktomcat.demo.redis.redisson.lock.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jdktomcat.demo.redis.redisson.lock.model.WithdrawOrder;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
+@Mapper
 public interface WithdrawOrderMapper extends BaseMapper<WithdrawOrder> {
+    @Select("select OrderId from typayv2.t_withdraw_order where find_in_set(OrderId, #{orderIds}) for update ")
+    List<String> queryListForUpdate(String orderIds);
 }
+
