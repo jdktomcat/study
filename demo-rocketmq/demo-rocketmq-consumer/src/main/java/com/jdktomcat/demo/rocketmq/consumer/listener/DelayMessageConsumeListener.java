e639872a11975910e890cba221c2c1b300549f08
timmy <jstimmy@itcom888.com>
2024-02-29 15:59:02 +0800

    RocketMQ showcase

--- demo-rocketmq/demo-rocketmq-consumer/src/main/java/com/jdktomcat/demo/rocketmq/consumer/listener/DelayMessageConsumeListener.java
+++ demo-rocketmq/demo-rocketmq-consumer/src/main/java/com/jdktomcat/demo/rocketmq/consumer/listener/DelayMessageConsumeListener.java
@@ -1,7 +1,6 @@
 package com.jdktomcat.demo.rocketmq.consumer.listener;
 
 import com.jdktomcat.demo.rocketmq.consumer.component.RedisComponent;
-import com.jdktomcat.demo.rocketmq.consumer.constant.RocketMQConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.rocketmq.common.message.MessageExt;
@@ -23,24 +22,19 @@ public class DelayMessageConsumeListener extends BaseConsumerListener {
 
     @Override
     protected void consume(String tag, String topic, MessageExt messageExt) {
+        String key = messageExt.getKeys();
         try {
-            log.info("----------延迟消息处理start----------");
-            log.info("延迟消息消费原始內容:{}", toString(messageExt));
             String body = new String(messageExt.getBody(), StandardCharsets.UTF_8).trim();
-            if (tag.equals(RocketMQConstant.DELAY_MESSAGE_TOPIC_TAG_VOUCHER)) {
-                if (StringUtils.isNotEmpty(body)) {
-                    Object value = redisComponent.get(body);
-                    String valueString = (value == null ? "" : value.toString());
-                    if (StringUtils.isEmpty(valueString)) {
-                        redisComponent.setIfAbsent(body,body,3, TimeUnit.SECONDS);
-                    }
-                    log.warn("商户订单{}银行回单编码验证确认超时", body);
+            if (StringUtils.isNotEmpty(body)) {
+                Object value = redisComponent.get(body);
+                String valueString = (value == null ? "" : value.toString());
+                if (StringUtils.isEmpty(valueString)) {
+                    redisComponent.setIfAbsent(body,body,3, TimeUnit.SECONDS);
                 }
-            } else {
-                log.warn("该延迟消息tag:{}没有对应的处理逻辑！", tag);
             }
+            log.warn("message :{} handle success！", key);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            log.error("message handle occur exception!{}",key,e);
         }
     }
 }
