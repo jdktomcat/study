67cd1f5693490f48d51cb15c2f8a091681bf2422
timmy <jstimmy@itcom888.com>
2024-03-02 16:51:06 +0800

    showcase

--- demo-redis-delay/demo-redis-delay-keyspace-notify/src/main/java/com/jdktomcat/demo/redis/delay/keyspace/notify/RedisDelayKeyspaceNotifyApplication.java
+++ demo-redis-delay/demo-redis-delay-keyspace-notify/src/main/java/com/jdktomcat/demo/redis/delay/keyspace/notify/RedisDelayKeyspaceNotifyApplication.java
@@ -2,6 +2,7 @@ package com.jdktomcat.demo.redis.delay.keyspace.notify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author ZF-Timmy
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description 类描述：
  * @date 2023/11/13
  */
+@EnableScheduling
 @SpringBootApplication
 public class RedisDelayKeyspaceNotifyApplication {
     public static void main(String[] args) {
