9b5297187fca8eba3e5f138eda027a32d444d9e8
timmy <jstimmy@itcom888.com>
2024-03-02 16:10:18 +0800

    showcase

--- demo-redis-delay/demo-redis-delay-keyspace-notify/src/main/java/com/jdktomcat/demo/redis/delay/keyspace/notify/scheduled/RedisClusterNodeFactory.java
+++ demo-redis-delay/demo-redis-delay-keyspace-notify/src/main/java/com/jdktomcat/demo/redis/delay/keyspace/notify/scheduled/RedisClusterNodeFactory.java
@@ -55,49 +55,50 @@ public class RedisClusterNodeFactory {
     
     @Scheduled(cron = "0/5 * * * * ?")
     public void refreshClusterNode() {
-            RedisClusterConnection clusterConnection = redisConnectionFactory.getClusterConnection();
-            Iterable<RedisClusterNode> redisClusterNodes = clusterConnection.clusterGetNodes();
-            // 用于获取当前的master节点信息
-            HashMap<String,String> currentMasterNodeMap = new HashMap<>();
-            redisClusterNodes.forEach(redisClusterNode -> {
-                if (redisClusterNode.isMaster()) {
-                    String clusterNodeName = "clusterNodeName" + redisClusterNode.hashCode();
-                    String hostPort = redisClusterNode.getHost() + ":" + redisClusterNode.getPort();
-                    currentMasterNodeMap.put(clusterNodeName, hostPort);
-                    // 不进行重复创建监听
-                    if (masterNodeMap.containsKey(clusterNodeName)) {
-                        return;
-                    }
-                    masterNodeMap.put(clusterNodeName, hostPort);
-                    Config config = new Config();
-                    SingleServerConfig singleServerConfig = config.useSingleServer();
-                    singleServerConfig.setAddress("redis://" + redisClusterNode.getHost() + ":" + redisClusterNode.getPort());
-                    singleServerConfig.setPassword(rPwd);
-                    singleServerConfig.setKeepAlive(true);
-                    singleServerConfig.setDatabase(0);
-                    singleServerConfig.setClientName(serverName);
-                    RedisConnectionFactory connectionFactory = new RedissonConnectionFactory(Redisson.create(config));
-                    RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-                    container.setConnectionFactory(connectionFactory);
-                    container.setTaskExecutor(taskAsyncPool);
-                    // 设置监听的Topic
-                    ChannelTopic channelTopic = new ChannelTopic("__keyevent@0__:expired");
-                    // 设置监听器
-                    container.addMessageListener(messageListener, channelTopic);
-                    log.info("设置服务器节点监听器host：{}，port:{}" ,redisClusterNode.getHost(), redisClusterNode.getPort());
-                    // 必须调用，否则空指针异常
-                    container.afterPropertiesSet();
-                    // 启动监控
-                    container.start();
+        log.info("refreshClusterNode run");
+        RedisClusterConnection clusterConnection = redisConnectionFactory.getClusterConnection();
+        Iterable<RedisClusterNode> redisClusterNodes = clusterConnection.clusterGetNodes();
+        // 用于获取当前的master节点信息
+        HashMap<String,String> currentMasterNodeMap = new HashMap<>();
+        redisClusterNodes.forEach(redisClusterNode -> {
+            if (redisClusterNode.isMaster()) {
+                String clusterNodeName = "clusterNodeName" + redisClusterNode.hashCode();
+                String hostPort = redisClusterNode.getHost() + ":" + redisClusterNode.getPort();
+                currentMasterNodeMap.put(clusterNodeName, hostPort);
+                // 不进行重复创建监听
+                if (masterNodeMap.containsKey(clusterNodeName)) {
+                    return;
                 }
-            });
+                masterNodeMap.put(clusterNodeName, hostPort);
+                Config config = new Config();
+                SingleServerConfig singleServerConfig = config.useSingleServer();
+                singleServerConfig.setAddress("redis://" + redisClusterNode.getHost() + ":" + redisClusterNode.getPort());
+                singleServerConfig.setPassword(rPwd);
+                singleServerConfig.setKeepAlive(true);
+                singleServerConfig.setDatabase(0);
+                singleServerConfig.setClientName(serverName);
+                RedisConnectionFactory connectionFactory = new RedissonConnectionFactory(Redisson.create(config));
+                RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+                container.setConnectionFactory(connectionFactory);
+                container.setTaskExecutor(taskAsyncPool);
+                // 设置监听的Topic
+                ChannelTopic channelTopic = new ChannelTopic("__keyevent@0__:expired");
+                // 设置监听器
+                container.addMessageListener(messageListener, channelTopic);
+                log.info("设置服务器节点监听器host：{}，port:{}" ,redisClusterNode.getHost(), redisClusterNode.getPort());
+                // 必须调用，否则空指针异常
+                container.afterPropertiesSet();
+                // 启动监控
+                container.start();
+            }
+        });
 
-            // 比较2个map中的值，去除不存在的值
-            masterNodeMap.forEach((key, value) -> {
-                if (!currentMasterNodeMap.containsKey(key)) {
-                    log.info("去除不存在的服务器节点:" + value);
-                    masterNodeMap.remove(key);
-                }
-            });
+        // 比较2个map中的值，去除不存在的值
+        masterNodeMap.forEach((key, value) -> {
+            if (!currentMasterNodeMap.containsKey(key)) {
+                log.info("去除不存在的服务器节点:" + value);
+                masterNodeMap.remove(key);
+            }
+        });
     }
 }
