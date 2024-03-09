ea52bd04d8286a25c4b3fbb1371796b9890214f8
timmy <jstimmy@itcom888.com>
2024-03-09 19:01:47 +0800

    showcase

--- demo-snowflake/demo-snowflake-generate/src/main/java/com/jdktomcat/demo/snowflake/generate/service/impl/WithdrawOrderServiceImpl.java
+++ demo-snowflake/demo-snowflake-generate/src/main/java/com/jdktomcat/demo/snowflake/generate/service/impl/WithdrawOrderServiceImpl.java
@@ -1,40 +1,36 @@
 package com.jdktomcat.demo.snowflake.generate.service.impl;
 
-import com.jdktomcat.demo.snowflake.generate.mapper.WithdrawOrderMapper;
-import com.jdktomcat.demo.snowflake.generate.model.WithdrawOrder;
 import com.jdktomcat.demo.snowflake.generate.service.IWithdrawOrderService;
 import com.jdktomcat.demo.snowflake.generate.util.SnowflakeIdWorker;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @Service
 public class WithdrawOrderServiceImpl implements IWithdrawOrderService {
 
-    @Autowired
-    private WithdrawOrderMapper withdrawOrderMapper;
+    private ExecutorService executors = Executors.newFixedThreadPool(5);
+    private static Set<Long> set = new CopyOnWriteArraySet<>();
 
     @Override
     @Transactional
     public String insert() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYYMMDDHHmmss");
-        for(int i=0;i<100;i++){
-            WithdrawOrder withdrawOrder = new WithdrawOrder();
-            withdrawOrder.setId(SnowflakeIdWorker.createTableId());
-            withdrawOrder.setOrderId("JSTWZORDER"+ simpleDateFormat.format(new Date())+ RandomStringUtils.randomAlphabetic(5).toUpperCase());
-            withdrawOrder.setOrderStatus(1);
-            withdrawOrder.setParentOrderStatus(1);
-            withdrawOrder.setMerchantOrderId("JSTWZMO"+ simpleDateFormat.format(new Date())+ RandomStringUtils.randomAlphabetic(5).toUpperCase());
-            withdrawOrder.setParentOrderStatus(1);
-            withdrawOrder.setCreateTime(new Date());
-            int save = withdrawOrderMapper.insert(withdrawOrder);
-            log.info("insert order:{} result:{}", withdrawOrder, save);
+        for(int i=0;i<5;i++){
+            executors.submit(() -> {
+                for(int index=0;index<10000;index++){
+                    Long id = SnowflakeIdWorker.createTableId();
+                    if(set.contains(id)){
+                        log.error("生成id异常！：{}",id);
+                    }
+                    set.add(id);
+                }
+            });
         }
         return "OK";
     }
